9200f85b14f58f7feb52aefe56656f16c92f95d5
Richard Pope <richard.pope@just-eat.com>
2016-10-10 15:45:08 +0100

    Initial commit

---
+++ SpecflowExample.Tests/FeatureOne.feature
@@ -0,0 +1,21 @@
+﻿@featuretag
+Feature: FeatureOne
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario Outline: Some scenario outline
+	Given I have entered 50.5 into the calculator
+	And I have entered 70.3 into the calculator
+	When I press add
+	Then the result should be "<total>" on the screen
+
+	@en-gb
+	Examples:
+	| total |
+	| 120.8 |
+
+	@fr-fr
+	Examples:
+	| total |
+	| 120,8 |
---
+++ SpecflowExample.Tests/FeatureOneSteps.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecflowExample.Tests
+{
+    [Binding]
+    public class FeatureOneSteps
+    {
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            var featureTags = FeatureContext.Current.FeatureInfo.Tags;
+            var scenarioTags = ScenarioContext.Current.ScenarioInfo.Tags;
+
+            Assert.IsNotEmpty(featureTags, "Feature tags list was empty");
+            Assert.IsNotEmpty(scenarioTags, "Scenario tags list was empty");
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be ""(.*)"" on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
